9e945188918a067799159028181a2ba1ff4e8703
Will Ezell <will@dotcms.com>
2018-05-11 18:04:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,237 @@
+# Navigation as a Service
+----
+This REST endpoint returns the navigation as defined in dotCMS as a json object
+
+
+## How to Use
+----
+
+Once installed, you can access this resource by (this assumes you are on localhost)
+
+`http://localhost:8080/api/v1/nav/?depth=5`
+
+or this, where you can specify a starting point for the nav
+
+`http://localhost:8080/api/v1/nav/about-us/?depth=2`
+
+
+## Returns
+```json
+{
+code: null,
+children: [
+{
+code: null,
+children: [
+{
+code: null,
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/flights/index",
+title: "Flight Search",
+type: "htmlpage",
+hash: 1165454360,
+target: "_self",
+order: 0
+}
+],
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/flights",
+title: "Flights",
+type: "folder",
+hash: 1078679786,
+target: "_self",
+order: 1
+},
+{
+code: null,
+children: [
+{
+code: null,
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/holidays/index",
+title: "View All Holidays",
+type: "htmlpage",
+hash: 533134847,
+target: "_self",
+order: 1
+},
+{
+code: null,
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/holidays/great-britain",
+title: "Great Britain",
+type: "folder",
+hash: 414222496,
+target: "_self",
+order: 2
+},
+{
+code: null,
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/holidays/greece",
+title: "Greece",
+type: "folder",
+hash: 1297463962,
+target: "_self",
+order: 3
+}
+],
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/holidays",
+title: "Holidays",
+type: "folder",
+hash: 1252833965,
+target: "_self",
+order: 2
+},
+{
+code: null,
+children: [
+{
+code: null,
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/destinations/index",
+title: "View All Desintations",
+type: "htmlpage",
+hash: 956597230,
+target: "_self",
+order: 0
+}
+],
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/destinations",
+title: "Destinations",
+type: "folder",
+hash: 1464022994,
+target: "_self",
+order: 3
+},
+{
+code: null,
+children: [
+{
+code: null,
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/deals/index",
+title: "View All Deals",
+type: "htmlpage",
+hash: 996016417,
+target: "_self",
+order: 0
+}
+],
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/deals",
+title: "Deals",
+type: "folder",
+hash: 2014676549,
+target: "_self",
+order: 4
+},
+{
+code: null,
+children: [
+{
+code: null,
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/cruise/index",
+title: "Search Cruises",
+type: "htmlpage",
+hash: 522766879,
+target: "_self",
+order: 1
+}
+],
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/cruise",
+title: "Cruise",
+type: "folder",
+hash: 1685906543,
+target: "_self",
+order: 5
+},
+{
+code: null,
+children: [
+{
+code: null,
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/city-breaks/index",
+title: "Browse All City Breaks",
+type: "htmlpage",
+hash: 2071527776,
+target: "_self",
+order: 1
+},
+{
+code: null,
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/city-breaks/rome",
+title: "Rome",
+type: "htmlpage",
+hash: 146159810,
+target: "_self",
+order: 2
+}
+],
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/city-breaks",
+title: "City Breaks",
+type: "folder",
+hash: 69010192,
+target: "_self",
+order: 6
+},
+{
+code: null,
+children: [
+{
+code: null,
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/travel-advice/index",
+title: "Travel Advice",
+type: "htmlpage",
+hash: 1260568438,
+target: "_self",
+order: 1
+},
+{
+code: null,
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/travel-advice/travel-safety",
+title: "Travel Safety",
+type: "htmlpage",
+hash: 2019804531,
+target: "_self",
+order: 2
+}
+],
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/travel-advice",
+title: "Travel Advice",
+type: "folder",
+hash: 1244547558,
+target: "_self",
+order: 7
+}
+],
+host: "e884ce2a-41fc-4a50-a4bf-575db001abd4",
+href: "/System foldersystem folder",
+title: "System folder",
+type: "folder",
+hash: 1769227458,
+target: "_self",
+order: 0
+}
+```
+
+
+## Authentication
+----
+This API supports the same REST auth infrastructure as other 
+rest apis in dotcms. There are 4 ways to authenticate.
+
+* user/xxx/password/yyy in the URI
+* basic http/https authentication (base64 encoded)
+* DOTAUTH header similar to basic auth and base64 encoded, e.g. setHeader("DOTAUTH", base64.encode("admin@dotcms.com:admin"))
+* Session based (form based login) for frontend or backend logged in user
---
+++ src/main/java/com/dotcms/plugin/rest/NavResource.java
@@ -0,0 +1,125 @@
+package com.dotcms.plugin.rest;
+
+import com.dotcms.repackage.javax.ws.rs.GET;
+import com.dotcms.repackage.javax.ws.rs.Path;
+import com.dotcms.repackage.javax.ws.rs.PathParam;
+import com.dotcms.repackage.javax.ws.rs.Produces;
+import com.dotcms.repackage.javax.ws.rs.QueryParam;
+import com.dotcms.repackage.javax.ws.rs.core.Context;
+import com.dotcms.repackage.javax.ws.rs.core.MediaType;
+import com.dotcms.repackage.javax.ws.rs.core.Response;
+import com.dotcms.rest.InitDataObject;
+import com.dotcms.rest.WebResource;
+import com.dotcms.rest.exception.mapper.ExceptionMapperUtil;
+
+import com.dotmarketing.beans.Host;
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.business.PermissionLevel;
+import com.dotmarketing.business.web.WebAPILocator;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotRuntimeException;
+import com.dotmarketing.exception.DotSecurityException;
+import com.dotmarketing.util.Config;
+import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.VelocityUtil;
+import com.dotmarketing.viewtools.navigation.NavResult;
+import com.dotmarketing.viewtools.navigation.NavTool;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.velocity.tools.view.context.ChainedContext;
+import org.apache.velocity.tools.view.context.ViewContext;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.liferay.portal.model.User;
+
+@Path("/v1/nav")
+public class NavResource  {
+
+    private final WebResource webResource = new WebResource();
+
+    @Produces({MediaType.APPLICATION_JSON, "application/javascript"})
+	@GET
+	@Path("/{uri: .*}")
+    public Response loadJson(@Context final HttpServletRequest request, @Context final HttpServletResponse response,
+            @PathParam("uri") final String uri, @QueryParam("depth") final int depth) {
+        System.err.println("NavResource hit");
+        final InitDataObject auth = webResource.init(false, request, true);
+        final User user = auth.getUser();
+
+        Response res = null;
+        try {
+            Host h =WebAPILocator.getHostWebAPI().getCurrentHost(request);
+            APILocator.getPermissionAPI().checkPermission(h, PermissionLevel.READ, user);
+            
+            ViewContext ctx = new ChainedContext(VelocityUtil.getBasicContext(), request, response, Config.CONTEXT);
+
+            final String path = (!uri.startsWith("/")) ? "/" + uri : uri;
+
+            NavTool tool = new NavTool();
+            tool.init(ctx);
+            NavResult nav = tool.getNav(path);
+            
+            Map<String, Object> navMap = (nav!=null) ? navToMap(nav, depth, 1) : new HashMap<>();
+
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(navMap);
+
+            final Response.ResponseBuilder responseBuilder = Response.ok(json);
+
+            responseBuilder.header("Access-Control-Expose-Headers", "Authorization");
+            responseBuilder.header("Access-Control-Allow-Headers",
+                    "Origin, X-Requested-With, " + "Content-Type, " + "Accept, Authorization");
+
+
+            res = responseBuilder.build();
+        } catch (JsonProcessingException e) {
+            final String errorMsg = "An error occurred when generating the JSON response (" + e.getMessage() + ")";
+            Logger.error(this, e.getMessage(), e);
+            res = ExceptionMapperUtil.createResponse(null, errorMsg);
+        } catch (DotSecurityException e) {
+            final String errorMsg = "The user does not have the required permissions (" + e.getMessage() + ")";
+            Logger.error(this, errorMsg, e);
+            throw new DotRuntimeException(e);
+        } catch (DotDataException e) {
+            final String errorMsg = "An error occurred when accessing the page information (" + e.getMessage() + ")";
+            Logger.error(this, e.getMessage(), e);
+            res = ExceptionMapperUtil.createResponse(null, errorMsg);
+        } catch (Exception e) {
+            final String errorMsg = "An internal error occurred (" + e.getMessage() + ")";
+            Logger.error(this, errorMsg, e);
+            res = ExceptionMapperUtil.createResponse(e, Response.Status.INTERNAL_SERVER_ERROR);
+        }
+        return res;
+	}    
+	private Map<String, Object> navToMap(final NavResult nav, final int maxDepth, final int currentDepth) throws Exception {
+
+        Map<String, Object> navMap = new HashMap<String, Object>();
+        navMap.put("title", nav.getTitle());
+        navMap.put("target", nav.getTarget());
+        navMap.put("code", nav.getCodeLink());
+        navMap.put("host", nav.getHostId());
+        navMap.put("href", nav.getHref());
+        navMap.put("order", nav.getOrder());
+        navMap.put("type", nav.getType());
+        navMap.put("hash", nav.hashCode());
+        
+        if (currentDepth < maxDepth) {
+            List<Map<String, Object>> childs = new ArrayList<>();
+            for (NavResult child : nav.getChildren()) {
+                int startDepth=currentDepth;
+                childs.add(navToMap(child, maxDepth, ++startDepth));
+            }
+            navMap.put("children", childs);
+        }
+
+        return navMap;
+    }
+}
